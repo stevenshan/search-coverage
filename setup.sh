3f4d10b95ef9c51b44ae3eb151a6892faf013f7b
Steven Shan <steven.shan99@gmail.com>
2018-03-04 14:49:48 -0500

    fixed syntax errors in setup file

--- setup.sh
+++ setup.sh
@@ -7,35 +7,35 @@ trap "exit 1" TERM
 export TOP_PID=$$
 
 # first argument should be path to cloned Unreal Engine source
-if [ -z "$1" ] then
+if [ -z "$1" ]; then
 	echo "Need path to Unreal Engine source"
 	exit
 fi
 
 # make sure the directory exists
 check_dir () {
-	if [ ! -d "$1" ] then
-		echo "Invalid path given"
+	if [ ! -d "$1" ]; then
+		echo "Invalid path given: ${1}"
 		kill -s TERM $TOP_PID
 	fi
 }
 
 target="${1%/}/Engine/"
 
-check_dir target
+check_dir $target
 
 targetone="${target}Build/BatchFiles/Linux/"
 targettwo="${target}Source/Programs/UnrealBuildTool/Linux"
-check_dir targetone
-check_dir targettwo
+check_dir $targetone
+check_dir $targettwo
 
 # move modified setup files to Unreal folder
 
 # replace all instances of clang-3.* to clang-3.5
-mv setup/UnrealEngine/Setup.sh targetone
+mv setup/UnrealEngine/Setup.sh $targetone
 
 # change CLANG_TO_USE=`which clang` to CLANG_TO_USE=`clang-3.5`
-mv setup/UnrealEngine/BuildThirdPart.sh targetone
+mv setup/UnrealEngine/BuildThirdParty.sh $targetone
 
 # force use of clang-3.5
-mv setup/UnrealEngine/LinuxToolChain.cs targettwo
+mv setup/UnrealEngine/LinuxToolChain.cs $targettwo
