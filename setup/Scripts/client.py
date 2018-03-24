0918bcd3004ac7901da6b3d31c0bd4d42bb4c1ec
Steven Shan <steven.shan99@gmail.com>
2018-03-24 17:21:20 -0400

    changed socket select to separate threads for windows compatibility

--- setup/Scripts/client.py
+++ setup/Scripts/client.py
@@ -1,27 +1,38 @@
 # telnet program example
 import socket, select, string, sys
 
+from threading import *
+
 import pickle
+
+def send_msg(sock):
+    while True:
+        data = sys.stdin.readline()
+        data = (((0, None),(1, None)), ("test", [1, 2, 3]))
+        sock.send(pickle.dumps(data))
+        
+def recv_msg(sock):
+    while True:
+        data = sock.recv(4096)
+        sys.stdout.write("<Server> " + data.decode("utf-8"))
  
 #main function
 if __name__ == "__main__":
      
     if (len(sys.argv) == 1):
-        print('Usage : python client.py hostname port')
-        print("Using default values localhost and port 5000")
         host = "localhost"
-        port = 5007
+        port = 4000
+        print('Usage : python client.py hostname ' + host)
+        print("Using default values localhost and port " + str(port))
     elif(len(sys.argv) < 3) :
         print('Usage : python client.py hostname port')
         sys.exit()
     else:
-	    host = sys.argv[1]
-	    port = int(sys.argv[2])
+        host = sys.argv[1]
+        port = int(sys.argv[2])
 
-     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-     
+    
     # connect to remote host
     try :
         s.connect((host, port))
@@ -31,25 +42,6 @@ if __name__ == "__main__":
      
     print('Connected to remote host. Start sending messages')
 
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [], 0)
-         
-        for sock in read_sockets:
-            #incoming message from remote server
-            if sock == s:
-                data = sock.recv(4096)
-                if not data :
-                    print('\nDisconnected from chat server')
-                    sys.exit()
-                else :
-                    # print data
-                    sys.stdout.write("<Server> " + data.decode("utf-8"))
-             
-            #user entered a message
-            else :
-                msg = sys.stdin.readline()
-                data = (((0, None),(1, None)), ("test", [1, 2, 3]))
-                s.send(pickle.dumps(data))
+    Thread(target=send_msg, args=(s,)).start()  
+    Thread(target=recv_msg, args=(s,)).start()
+       
